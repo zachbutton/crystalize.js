d62cc4620b57f508bce925915ab1670afd5ef78d
Zach Button <zach1188@gmail.com>
2023-09-13 21:49:25 -0700

    Add coverage badge github action

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    transform: {
+        '^.+\\.ts$': 'ts-jest',
+    },
+    testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
+    moduleFileExtensions: ['js', 'ts'],
+    coverageReporters: ['json-summary'],
+    coverageReporters: ['json-summary'],
+};
